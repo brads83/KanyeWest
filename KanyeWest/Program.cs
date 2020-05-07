74d6c474ffe1208a5e62211d25b6cbb2840ecc03
brads83 <bradsorrells73@gmail.com>
2020-05-07 14:28:52 -0500

    API complete

--- KanyeWest/Program.cs
+++ KanyeWest/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 
 namespace KanyeWest
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 5; i++)
+            {
+                KanyeQuote();
+                RonQuote();
+                ChuckQuote();
+            }
         }
+        public static void KanyeQuote()
+        {
+            //API url
+            var kanyeURL = " https://api.kanye.rest";
+
+            //Allows us to call API
+            var client = new HttpClient();
+
+            //Stores the JSON response in a variable
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            //Parses through the response we received to get value associated with the NAME quote
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+        }
+
+        public static void RonQuote()
+        {
+            //Ron Swanson Url
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var client = new HttpClient();
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron Swanson: {ronQuote}");
+        }
+        
+        public static void ChuckQuote()
+        {
+            var chuckURL = "https://api.chucknorris.io/jokes/random";
+
+            var client = new HttpClient();
+
+            var chuckResponse = client.GetStringAsync(chuckURL).Result;
+
+            var chuckQuote = JObject.Parse(chuckResponse).GetValue("value").ToString();
+
+            Console.WriteLine($"Chuck Norris: {chuckQuote}");
+        }
+
+
+
+        
     }
 }
